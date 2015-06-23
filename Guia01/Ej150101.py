c93dd73baec9e6316963aa142cada7b16b98fcdc
Diego Serrano <diegojserrano@gmail.com>
2015-06-22 22:42:16 -0300

    Guia 1 - Ejercicio 1

--- Guia01/Ej150101.py
+++ Guia01/Ej150101.py
@@ -1,9 +1,5 @@
 __author__ = 'Catedra Algoritmos y Estructuras de Datos'
 
-# Desarrolle un programa para calcular el área de un triángulo, cargando por teclado el valor de la base,
-#  pero sabiendo que su altura es igual al cuadrado de la base.
-# (Observar que la altura no es un dato... sólo se indica la forma de calcularla de acuerdo a la base que sí es un dato)
-
 # Titulos y Lectura de datos
 print('Ejercicio A - Calculo del area de un triangulo')
 base = float(input('Ingrese la base del triangulo: '))
